458379e0998622acb4b078bcb9c1f769708db5f6
IvanR96 <irios96@hotmail.com>
2023-12-10 22:10:21 -0800

    factory / normal constructor examples

---
+++ script.js
@@ -0,0 +1,16 @@
+
+//constructor
+const User = function(name){
+    this.name = name;
+    this.discordName = '@' + name;
+
+}
+
+//factory function no need for new keyword
+function createUser(name){
+    const discordName = '@' + name;
+    return {name, discordName};
+}
+
+
+
